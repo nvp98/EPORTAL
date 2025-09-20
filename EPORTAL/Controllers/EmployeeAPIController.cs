b380e0c8876c953c0229b5edc3c0a91729c9153e
nvp98 <vinhphuoc1892@gmail.com>
2025-09-20 08:50:25 +0700

    fix bug version and change password HPPY

--- EPORTAL/Controllers/EmployeeAPIController.cs
+++ EPORTAL/Controllers/EmployeeAPIController.cs
@@ -72,7 +72,7 @@ namespace EPORTAL.Controllers
         }
         public ActionResult Sync()
         {
-            string MaNV, sMaNV;
+            string MaNV, sMaNV,sMaNVPY;
             int IDViTri, IDPhongBan;
             int dtc = 0;
             string msg = "";
@@ -91,11 +91,12 @@ namespace EPORTAL.Controllers
                             {
                                 MaNV = item.manv;
                                 sMaNV = MaNV.Substring(0, 4);
+                                sMaNVPY = MaNV.Substring(0, 5);
 
                                 var rsnv = LNV.Where(x => x.MaNV == MaNV).FirstOrDefault();
                                 if (rsnv == null)
                                 {
-                                    if (sMaNV == "HPDQ" && MaNV.Length==9)
+                                    if (sMaNV == "HPDQ" || sMaNVPY == "CBGPY")
                                     {
                                         ObjectParameter IDPhongBanout = new ObjectParameter("IDPhongBan", typeof(int));
                                         ObjectParameter IDViTriout = new ObjectParameter("IDViTri", typeof(int));
