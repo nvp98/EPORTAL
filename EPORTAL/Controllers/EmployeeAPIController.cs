451c2d55f4781425696ed4f09346892226ef6ad3
phuocnv1 <vinhphuoc1892@gmail.com>
2026-06-30 10:14:01 +0700

    update sync NMN

--- EPORTAL/Controllers/EmployeeAPIController.cs
+++ EPORTAL/Controllers/EmployeeAPIController.cs
@@ -77,8 +77,8 @@ namespace EPORTAL.Controllers
                 //Getdata(json);
                 //Json khong dung chuan
                 //=>Nen cat chuoi, cat chuoi the nay cu chuoi qua
-              /*  json = json.Remove(0, 36);
-                json = json.Replace("}]}", "}]");*/
+                /*  json = json.Remove(0, 36);
+                  json = json.Replace("}]}", "}]");*/
                 var list = JsonConvert.DeserializeObject<Employees_API>(json);
                 return list.data.ToList();
             }
@@ -115,7 +115,7 @@ namespace EPORTAL.Controllers
         }
         public ActionResult Sync()
         {
-            string MaNV, sMaNV,sMaNVPY, sMaNV3;
+            string MaNV, sMaNV, sMaNVPY, sMaNV3;
             int IDViTri, IDPhongBan;
             int dtc = 0;
             string msg = "";
@@ -123,99 +123,99 @@ namespace EPORTAL.Controllers
             {
 
                 List<NhanVien> LNV = _db.NhanViens.ToList();
-               /* if (MyAuthentication.Username == "HPDQ12806" || MyAuthentication.Username == "HPDQ18461")
-                {*/
-                    List<Employees_API.Employee> listNV = GetAPI();
-                    if (listNV.Count > 0)
+                /* if (MyAuthentication.Username == "HPDQ12806" || MyAuthentication.Username == "HPDQ18461")
+                 {*/
+                List<Employees_API.Employee> listNV = GetAPI();
+                if (listNV.Count > 0)
+                {
+                    foreach (var item in listNV)
                     {
-                        foreach (var item in listNV)
+                        if (item.manv != null)
                         {
-                            if (item.manv != null)
-                            {
-                                MaNV = item.manv;
-                                sMaNV = MaNV.Substring(0, 4);
-                                sMaNVPY = MaNV.Substring(0, 5);
-                                sMaNV3 = MaNV.Substring(0, 3);
+                            MaNV = item.manv;
+                            sMaNV = MaNV.Substring(0, 4);
+                            sMaNVPY = MaNV.Substring(0, 5);
+                            sMaNV3 = MaNV.Substring(0, 3);
 
                             var rsnv = LNV.Where(x => x.MaNV == MaNV).FirstOrDefault();
-                                if (rsnv == null)
+                            if (rsnv == null)
+                            {
+                                if (sMaNV == "HPDQ" || sMaNVPY == "CBGPY" || sMaNVPY == "KCNHT" || sMaNV3 == "RAY" || sMaNV3 == "HMN")
                                 {
-                                    if (sMaNV == "HPDQ" || sMaNVPY == "CBGPY" || sMaNVPY == "KCNHT" || sMaNV3=="RAY")
+                                    ObjectParameter IDPhongBanout = new ObjectParameter("IDPhongBan", typeof(int));
+                                    ObjectParameter IDViTriout = new ObjectParameter("IDViTri", typeof(int));
+                                    IDPhongBan = GetIDPhongBan(item.phongban);
+                                    if (IDPhongBan == 0)
                                     {
-                                        ObjectParameter IDPhongBanout = new ObjectParameter("IDPhongBan", typeof(int));
-                                        ObjectParameter IDViTriout = new ObjectParameter("IDViTri", typeof(int));
-                                        IDPhongBan = GetIDPhongBan(item.phongban);
-                                        if (IDPhongBan == 0)
-                                        {
-                                            _db.PhongBan_insert(IDPhongBanout, item.phongban);
-                                            IDPhongBan = Convert.ToInt32(IDPhongBanout.Value);
-                                        }
-                                        IDViTri = GetIDViTri(item.vitri);
-                                        if (IDViTri == 0)
-                                        {
-                                            _db.Vitri_insert(IDViTriout, item.vitri);
-                                            IDViTri = Convert.ToInt32(IDViTriout.Value);
-                                        }
-                                        _db.Nhanvien_insert_API(MaNV, item.hoten, convertToUnSign(item.hoten), item.diachi, item.sodienthoai, DateTime.ParseExact(item.ngayvaolam, "dd/MM/yyyy", CultureInfo.InvariantCulture), IDPhongBan, item.tinhtranglamviec, IDViTri);
-                                        _db.NhanVienCCCD_Update(MaNV, item.cmnd);
+                                        _db.PhongBan_insert(IDPhongBanout, item.phongban);
+                                        IDPhongBan = Convert.ToInt32(IDPhongBanout.Value);
                                     }
-                                }
-                                else
-                                {
-                                    if (sMaNV == "HPDQ" || sMaNVPY == "CBGPY" || sMaNVPY == "KCNHT" || sMaNV3 == "RAY")
+                                    IDViTri = GetIDViTri(item.vitri);
+                                    if (IDViTri == 0)
                                     {
-                                        ObjectParameter IDPhongBanout = new ObjectParameter("IDPhongBan", typeof(int));
-                                        ObjectParameter IDViTriout = new ObjectParameter("IDViTri", typeof(int));
-                                        IDPhongBan = GetIDPhongBan(item.phongban);
-                                        if (IDPhongBan == 0)
-                                        {
-                                            _db.PhongBan_insert(IDPhongBanout, item.phongban);
-                                            IDPhongBan = Convert.ToInt32(IDPhongBanout.Value);
-                                        }
-                                        IDViTri = GetIDViTri(item.vitri);
-                                        if (IDViTri == 0)
-                                        {
-                                            _db.Vitri_insert(IDViTriout, item.vitri);
-                                            IDViTri = Convert.ToInt32(IDViTriout.Value);
-                                        }
-                                        if (rsnv.IDPhongBan != IDPhongBan || rsnv.IDViTri != IDViTri || rsnv.IDTinhTrangLV!=item.tinhtranglamviec)
-                                        {
-                                            _db.Nhanvien_update_API(MaNV, item.diachi, item.sodienthoai, IDPhongBan, item.tinhtranglamviec, IDViTri);
-                                            dtc++;
-                                        }
-                                        if(item.tinhtranglamviec==1)
-                                        {
-                                            var model = _db.AuthorizationContractors.Where(x => x.IDNhanVien == rsnv.ID).FirstOrDefault();
-                                            if (model == null)
-                                            {
-                                                _db.AuthorizationContractors_insert(rsnv.ID, 4, null);
-                                            }
-                                            var mkd=_db.KD_KyDuyet.Where(x=>x.IDNhanVien==rsnv.ID).FirstOrDefault();
-                                            if(mkd == null)
-                                            {
-                                                _db.KD_KyDuyet_insert(rsnv.ID, "",4);
-                                            }    
-                                        }
-                                        _db.NhanVienCCCD_Update(MaNV, item.cmnd);
-
+                                        _db.Vitri_insert(IDViTriout, item.vitri);
+                                        IDViTri = Convert.ToInt32(IDViTriout.Value);
                                     }
+                                    _db.Nhanvien_insert_API(MaNV, item.hoten, convertToUnSign(item.hoten), item.diachi, item.sodienthoai, DateTime.ParseExact(item.ngayvaolam, "dd/MM/yyyy", CultureInfo.InvariantCulture), IDPhongBan, item.tinhtranglamviec, IDViTri);
+                                    _db.NhanVienCCCD_Update(MaNV, item.cmnd);
                                 }
                             }
+                            else
+                            {
+                                if (sMaNV == "HPDQ" || sMaNVPY == "CBGPY" || sMaNVPY == "KCNHT" || sMaNV3 == "RAY" || sMaNV3 == "HMN")
+                                {
+                                    ObjectParameter IDPhongBanout = new ObjectParameter("IDPhongBan", typeof(int));
+                                    ObjectParameter IDViTriout = new ObjectParameter("IDViTri", typeof(int));
+                                    IDPhongBan = GetIDPhongBan(item.phongban);
+                                    if (IDPhongBan == 0)
+                                    {
+                                        _db.PhongBan_insert(IDPhongBanout, item.phongban);
+                                        IDPhongBan = Convert.ToInt32(IDPhongBanout.Value);
+                                    }
+                                    IDViTri = GetIDViTri(item.vitri);
+                                    if (IDViTri == 0)
+                                    {
+                                        _db.Vitri_insert(IDViTriout, item.vitri);
+                                        IDViTri = Convert.ToInt32(IDViTriout.Value);
+                                    }
+                                    if (rsnv.IDPhongBan != IDPhongBan || rsnv.IDViTri != IDViTri || rsnv.IDTinhTrangLV != item.tinhtranglamviec)
+                                    {
+                                        _db.Nhanvien_update_API(MaNV, item.diachi, item.sodienthoai, IDPhongBan, item.tinhtranglamviec, IDViTri);
+                                        dtc++;
+                                    }
+                                    if (item.tinhtranglamviec == 1)
+                                    {
+                                        var model = _db.AuthorizationContractors.Where(x => x.IDNhanVien == rsnv.ID).FirstOrDefault();
+                                        if (model == null)
+                                        {
+                                            _db.AuthorizationContractors_insert(rsnv.ID, 4, null);
+                                        }
+                                        var mkd = _db.KD_KyDuyet.Where(x => x.IDNhanVien == rsnv.ID).FirstOrDefault();
+                                        if (mkd == null)
+                                        {
+                                            _db.KD_KyDuyet_insert(rsnv.ID, "", 4);
+                                        }
+                                    }
+                                    _db.NhanVienCCCD_Update(MaNV, item.cmnd);
 
+                                }
+                            }
                         }
-                        if (dtc != 0)
-                        {
-                            msg = "Cập nhật thông tin được " + dtc + " nhân viên";
-                        }
-                        TempData["msgSuccess"] = "<script>alert('" + msg + "');</script>";
-                        return RedirectToAction("Index", "Account");
+
                     }
+                    if (dtc != 0)
+                    {
+                        msg = "Cập nhật thông tin được " + dtc + " nhân viên";
+                    }
+                    TempData["msgSuccess"] = "<script>alert('" + msg + "');</script>";
+                    return RedirectToAction("Index", "Account");
                 }
-           /* }
-            else
-            {
-                return RedirectToAction("", "Login");
-            }*/
+            }
+            /* }
+             else
+             {
+                 return RedirectToAction("", "Login");
+             }*/
             return View();
         }
     }
